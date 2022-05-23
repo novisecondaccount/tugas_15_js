e5ba7c89f96315c9ba914b4a0cc200e2dbb14a6e
novifitri <animangap@gmail.com>
2022-05-23 20:24:25 +0700

    init

---
+++ tugas15.js
@@ -0,0 +1,6 @@
+let AB = 8;
+let BC = 6;
+let AC = Math.sqrt(Math.pow(AB,2) + Math.pow(BC,2));
+console.log("Nilai AB : " +AB)
+console.log("Nilai BC : "+BC)
+console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah : " +AC)
